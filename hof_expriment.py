dfe311846b7bcfdedbd75685d407895d1d027f7b
Ivan Xu <info@chivalrystudio.com>
2021-05-03 14:52:01 -0700

    seems working

--- hof_expriment.py
+++ hof_expriment.py
@@ -1,78 +1,96 @@
+def n_body(init_func, update_func):
+    # This is the kernel for raw method
+    raw_kernel_str = '''
 import taichi as ti
-import taichi.lang
+import math
 
-ti.init(arch=ti.cpu)
+ti.init()
 
-
-def write_to_file(s):
-    f = open("created.py", 'w')
-    f.write(s)
-    f.close()
-
-
-DT = 1e-5
-DIM = 2
-NUM_MAX_PARTICLE = 1024
-num_particles = ti.field(dtype=ti.i32, shape=())
-particle_pos = ti.Vector.field(n=DIM, dtype=ti.f32)
-table = ti.root.dense(indices=ti.i, dimensions=NUM_MAX_PARTICLE)
-table.place(particle_pos)
-
+%s 
 
 @ti.func
 def alloc_particle():
     ret = ti.atomic_add(num_particles[None], 1)
     assert ret < NUM_MAX_PARTICLE
+    particle_mass[ret] = 0
     particle_pos[ret] = particle_pos[0] * 0
+    particle_vel[ret] = particle_pos[0] * 0
     return ret
 
 
-@ti.kernel
-def initialize(num_p: ti.i32):
-    for _ in range(num_p):
-        particle_id = alloc_particle()
-        particle_pos[particle_id] = ti.Vector([0.5, 0.5])
-
-
 @ti.func
-def gravity_func(distance):
-    """
-    Define which ever the equation used to compute gravity here
-    :param distance: the distance between things.
-    :return:
-    """
-    l2 = distance.norm_sqr() + 1e-3
-    return distance * (l2 ** ((-3) / 2))
-
-
-if __name__ == '__main__':
-    k_str = '''
-import taichi as ti
-
-
-@ti.func
-def get_gravity_at(n_par, par_pos, pos):
-    acc = par_pos[0] * 0
-    for i in range(n_par[None]):
-        acc += gravity_func(par_pos[i] - pos)
+def get_raw_gravity_at(pos):
+    acc = particle_pos[0] * 0
+    for i in range(num_particles[None]):
+        acc += particle_mass[i] * gravity_func(particle_pos[i] - pos)
     return acc
 
 
-# The O(N^2) kernel algorithm
 @ti.kernel
-def substep(n_par: ti.template(), par_pos: ti.template()):
-    for i in range(n_par[None]):
-        acceleration = get_gravity_at(n_par, par_pos, par_pos[i])
-        par_pos[i] += 0.001 * acceleration
-   
-    '''
-    write_to_file(k_str)
+def substep_raw():
+    for i in range(num_particles[None]):
+        acceleration = get_raw_gravity_at(particle_pos[i])
+        particle_vel[i] += acceleration * DT
+
+    for i in range(num_particles[None]):
+        particle_pos[i] += particle_vel[i] * DT
+
+
+%s
+    ''' % (init_func, update_func)
+
+    def write_to_file(s):
+        f = open("created.py", 'w')
+        f.write(s)
+        f.close()
+
+    write_to_file(raw_kernel_str)
     import created
 
-    initialize(2 ** 10)
+    created.initialize(2 ** 10)
 
-    test_kernel = taichi.lang.kernel(created.substep)
 
-    test_kernel(num_particles, particle_pos)
+if __name__ == '__main__':
+    update_str = '''
+    @ti.func
+    def gravity_func(distance):
+        """
+        Define which ever the equation used to compute gravity here
+        :param distance: the distance between things.
+        :return:
+        """
+        # --- The equation defined in the new n-body example
+        # (self**2).sum()
+        l2 = distance.norm_sqr() + 1e-3
+        return distance * (l2 ** ((-3) / 2))
+    '''
 
-    # print(test_kernel())
+    init_str = '''
+    DT = 1e-5
+    DIM = 2
+    NUM_MAX_PARTICLE = 32768  # 2^15
+    SHAPE_FACTOR = 1
+
+    particle_pos = ti.Vector.field(n=DIM, dtype=ti.f32)
+    particle_vel = ti.Vector.field(n=DIM, dtype=ti.f32)
+    particle_mass = ti.field(dtype=ti.f32)
+    particle_table = ti.root.dense(indices=ti.i, dimensions=NUM_MAX_PARTICLE)
+    particle_table.place(particle_pos).place(particle_vel).place(particle_mass)
+    num_particles = ti.field(dtype=ti.i32, shape=())
+
+    @ti.kernel
+    def initialize(num_p: ti.i32):
+        for _ in range(num_p):
+            particle_id = alloc_particle()
+
+            particle_mass[particle_id] = ti.random() * 1.4 + 0.1
+
+            a = ti.random() * math.tau
+            r = ti.sqrt(ti.random()) * 0.3
+            particle_pos[particle_id] = 0.5 + ti.Vector([ti.cos(a), ti.sin(a)]) * r
+    '''
+
+    # Pick your ingredient here
+    init = init_str
+    update = update_str
+    kernel = n_body(init, update)
