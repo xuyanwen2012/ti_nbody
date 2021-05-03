f800df748635150e79af38b2cb02981f4e4744d8
Ivan Xu <info@chivalrystudio.com>
2021-05-03 15:03:35 -0700

    seems working

--- hof_expriment.py
+++ hof_expriment.py
@@ -1,3 +1,6 @@
+import taichi as ti
+
+
 def n_body(init_func, update_func):
     # This is the kernel for raw method
     raw_kernel_str = '''
@@ -49,48 +52,75 @@ def substep_raw():
 
     created.initialize(2 ** 10)
 
+    return lambda _: created.substep_raw()
+
 
 if __name__ == '__main__':
     update_str = '''
-    @ti.func
-    def gravity_func(distance):
-        """
-        Define which ever the equation used to compute gravity here
-        :param distance: the distance between things.
-        :return:
-        """
-        # --- The equation defined in the new n-body example
-        # (self**2).sum()
-        l2 = distance.norm_sqr() + 1e-3
-        return distance * (l2 ** ((-3) / 2))
-    '''
+@ti.func
+def gravity_func(distance):
+    """
+    Define which ever the equation used to compute gravity here
+    :param distance: the distance between things.
+    :return:
+    """
+    # --- The equation defined in the new n-body example
+    # (self**2).sum()
+    l2 = distance.norm_sqr() + 1e-3
+    return distance * (l2 ** ((-3) / 2))
+'''
 
-    init_str = '''
-    DT = 1e-5
-    DIM = 2
-    NUM_MAX_PARTICLE = 32768  # 2^15
-    SHAPE_FACTOR = 1
+    init_circle_str = '''
+@ti.kernel
+def initialize(num_p: ti.i32):
+    for _ in range(num_p):
+        particle_id = alloc_particle()
 
-    particle_pos = ti.Vector.field(n=DIM, dtype=ti.f32)
-    particle_vel = ti.Vector.field(n=DIM, dtype=ti.f32)
-    particle_mass = ti.field(dtype=ti.f32)
-    particle_table = ti.root.dense(indices=ti.i, dimensions=NUM_MAX_PARTICLE)
-    particle_table.place(particle_pos).place(particle_vel).place(particle_mass)
-    num_particles = ti.field(dtype=ti.i32, shape=())
+        particle_mass[particle_id] = ti.random() * 1.4 + 0.1
 
-    @ti.kernel
-    def initialize(num_p: ti.i32):
-        for _ in range(num_p):
-            particle_id = alloc_particle()
+        a = ti.random() * math.tau
+        r = ti.sqrt(ti.random()) * 0.3
+        particle_pos[particle_id] = 0.5 + ti.Vector([ti.cos(a), ti.sin(a)]) * r
+'''
 
-            particle_mass[particle_id] = ti.random() * 1.4 + 0.1
+    init_uniform_str = '''
+@ti.kernel
+def initialize(num_p: ti.i32):
+    for _ in range(num_p):
+        particle_id = alloc_particle()
+        particle_mass[particle_id] = ti.random() * 1.4 + 0.1
+        particle_pos[particle_id] = ti.Vector([ti.random(), ti.random()])
+'''
 
-            a = ti.random() * math.tau
-            r = ti.sqrt(ti.random()) * 0.3
-            particle_pos[particle_id] = 0.5 + ti.Vector([ti.cos(a), ti.sin(a)]) * r
-    '''
+    # Maybe define list of particles here ???
 
-    # Pick your ingredient here
-    init = init_str
+    declare_tables_str = '''
+DT = 1e-5
+DIM = 2
+NUM_MAX_PARTICLE = 32768  # 2^15
+SHAPE_FACTOR = 1
+
+particle_pos = ti.Vector.field(n=DIM, dtype=ti.f32)
+particle_vel = ti.Vector.field(n=DIM, dtype=ti.f32)
+particle_mass = ti.field(dtype=ti.f32)
+particle_table = ti.root.dense(indices=ti.i, dimensions=NUM_MAX_PARTICLE)
+particle_table.place(particle_pos).place(particle_vel).place(particle_mass)
+num_particles = ti.field(dtype=ti.i32, shape=())
+
+'''
+
+    # Pick your ingredient for kernel here
+    init = declare_tables_str + init_circle_str
     update = update_str
     kernel = n_body(init, update)
+
+    # Renderer related
+    RES = (640, 480)
+    gui = ti.GUI('N-body Star', res=RES)
+
+    for _ in range(10000):
+        kernel(1)
+
+    # while gui.running:
+    #     gui.circles(particle_pos.to_numpy(), radius=2, color=0xfbfcbf)
+    #     gui.show()
